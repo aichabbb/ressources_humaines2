a0e6e773cba2c9f7d9af83f65604b297f070266f
aichabbb <65061771+aichabbb@users.noreply.github.com>
2022-08-01 01:19:52 +0100

    c

--- contrat_osi/wizard/trnspormation.py
+++ contrat_osi/wizard/trnspormation.py
@@ -57,9 +57,9 @@ class transformation(models.TransientModel):
                         'wage': rc.cantrat.wage,
                     })
 
-                    self.cantrat.write({'state': 'close',
-                            'date_end': today,
-                            })
+                    # self.cantrat.write({'state': 'close',
+                    #         'date_end': today,
+                    #         })
 
                     return {
                         "type": "ir.actions.act_window",
