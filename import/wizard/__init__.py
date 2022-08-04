a9e7739e83e5a775bd0a9d4294e62f134183d628
aichabbb <65061771+aichabbb@users.noreply.github.com>
2022-08-04 19:32:46 +0100

    c

---
+++ import/wizard/__init__.py
@@ -0,0 +1,2 @@
+from . import import_bank
+from . import payroll
---
+++ integration_contra/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': "import contra type",
+    'version': '15.0.1.0.0',
+    'summary': """""",
+    'description': """""",
+    'category': 'Generic Modules/Human Resources',
+    'live_test_url': '',
+    'author': 'aicha',
+    'company': '',
+    'maintainer': '',
+    'website': "",
+    'depends': ['hr', 'hr_contract', 'hr_contract_types'],
+
+    'data': [
+        'data/data_type_contra.xml',
+
+
+    ],
+
+    'license': "AGPL-3",
+    'installable': True,
+    'application': True,
+}
