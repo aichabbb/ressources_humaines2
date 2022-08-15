e323438cade4f4e0eb4cf552ee5d199f15f0c1da
aichabbb <65061771+aichabbb@users.noreply.github.com>
2022-08-15 09:44:44 +0100

    c

--- contrat_osi/models/conge.py
+++ contrat_osi/models/conge.py
@@ -18,38 +18,43 @@ class hr_leave(models.Model):
         today = fields.Date.today()
         len_C = int(len(config) - 1)
         list = []
-        conf = list[len_C]
-        if self.holiday_status_id == absence_justifies:
-            conge_justifies = self.env['hr.leave'].search([('holiday_status_id', '=', "absence justifies"),('employee_id', '=', self.employee_id.id)])
-            conge_justifies_nom_jus = self.env['hr.leave'].search([('holiday_status_id', '=', "absence nom justifies"),('employee_id', '=', self.employee_id.id)])
-            for cong in conge_justifies:
-                sun = cong.number_of_days
-                qty_produced = sum(conge_justifies.mapped('number_of_days'))
-                qty_produced_nom_justri = sum(conge_justifies_nom_jus.mapped('number_of_days'))
-                if qty_produced >= conf.plafond_des_absence_justifie:
-                    return {
-                        'name': _('conge'),
-                        'view_mode': 'form',
-                        'res_model': 'conge',
-                        'view_id': self.env.ref('contrat_osi.view_conge').id,
-                        'type': 'ir.actions.act_window',
-                        'target': 'new',
-                        'context': {'default_conge': self.id, },
-                    }
-                elif qty_produced_nom_justri >= conf.plafond_des_absence_non_justifie:
-                    return {
-                        'name': _('conge'),
-                        'view_mode': 'form',
-                        'res_model': 'conge',
-                        'view_id': self.env.ref('contrat_osi.view_conge').id,
-                        'type': 'ir.actions.act_window',
-                        'target': 'new',
-                        'context': {'default_conge': self.id, },
-                    }
+        for rc in config:
+            list.append(rc)
+            if rc.Age_la_retraite:
+                _logger.info('Device  is now disconnected UUUUUUUUUUUUUUUUUUUUUUUUUUUU%s', list)
 
-            # for move in self:
-            #     move.quantity_done = sum(move.mapped('move_line_ids.qty_done'))
-
-
+        # conf = list[len_C]
+        # if self.holiday_status_id == absence_justifies:
+        #     conge_justifies = self.env['hr.leave'].search([('holiday_status_id', '=', "absence justifies"),('employee_id', '=', self.employee_id.id)])
+        #     conge_justifies_nom_jus = self.env['hr.leave'].search([('holiday_status_id', '=', "absence nom justifies"),('employee_id', '=', self.employee_id.id)])
+        #     for cong in conge_justifies:
+        #         sun = cong.number_of_days
+        #         qty_produced = sum(conge_justifies.mapped('number_of_days'))
+        #         qty_produced_nom_justri = sum(conge_justifies_nom_jus.mapped('number_of_days'))
+        #         if qty_produced >= conf.plafond_des_absence_justifie:
+        #             return {
+        #                 'name': _('conge'),
+        #                 'view_mode': 'form',
+        #                 'res_model': 'conge',
+        #                 'view_id': self.env.ref('contrat_osi.view_conge').id,
+        #                 'type': 'ir.actions.act_window',
+        #                 'target': 'new',
+        #                 'context': {'default_conge': self.id, },
+        #             }
+        #         elif qty_produced_nom_justri >= conf.plafond_des_absence_non_justifie:
+        #             return {
+        #                 'name': _('conge'),
+        #                 'view_mode': 'form',
+        #                 'res_model': 'conge',
+        #                 'view_id': self.env.ref('contrat_osi.view_conge').id,
+        #                 'type': 'ir.actions.act_window',
+        #                 'target': 'new',
+        #                 'context': {'default_conge': self.id, },
+        #             }
+        #
+        #     # for move in self:
+        #     #     move.quantity_done = sum(move.mapped('move_line_ids.qty_done'))
+        #
+        #
 
 
